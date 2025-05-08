58579f77a13517a7debcfc72fb23125e0a0f511a
LopDor <149813107+radiopoweronline@users.noreply.github.com>
2025-05-08 11:46:58 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+window.streams = {
+    timeRefresh: 10000,
+    stations: [
+        {
+            name: "Radio Power",
+            hash: "",
+            description: "Con el poder de los clasicos",
+            logo: "assets1/radio_cover.webp",
+            album: "assets1/radio_cover.webp",
+            cover: "",
+            api: "https://api.zeno.fm/mounts/metadata/subscribe/dlvtl3hthyxvv",
+            stream_url: "https://stream.zeno.fm/dlvtl3hthyxvv",
+            tv_url: "",
+            server: "",
+            apps: {
+                android: "",
+                ios: "",
+            },
+        }
+    ]
+};
